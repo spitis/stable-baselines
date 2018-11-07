40fbb5c09dfbafc23dcf3ab0d1b0a0d4b504fccb
Takonan <takonan@gmail.com>
2018-11-07 17:49:39 -0500

    Added example of tensorboard logging for the goal gridworld training, just add path for logging

--- stable_baselines/simple_deepq/experiments/enjoy_goalgridworld.py
+++ stable_baselines/simple_deepq/experiments/enjoy_goalgridworld.py
@@ -1,3 +1,12 @@
+"""
+enjoy_goalgridworld.py
+
+Example usage:
+
+python enjoy_goalgridworld.py --model-type cnn --max-timestep 5000 --room-file room_5x5_empty
+
+"""
+
 import argparse
 
 import gym
@@ -13,9 +22,9 @@ def main(args):
 
     :param args: (ArgumentParser) the input arguments
     """
-    grid_file = 'room_5x5_empty.txt'
-    env = GoalGridWorldEnv(grid_size=5, max_step=12, grid_file=grid_file)
-    model_filename = "goalgridworld_model_{}_{}.pkl".format(args.model_type, args.max_timesteps)
+    grid_file = "{}.txt".format(args.room_file)
+    env = GoalGridWorldEnv(grid_size=9, max_step=12, grid_file=grid_file)
+    model_filename = "goalgridworld_model_{}_{}_{}.pkl".format(args.model_type, args.max_timesteps, args.room_file)
     model = DQN.load(model_filename, env, goal_space=env.observation_space.spaces['desired_goal'])
 
     max_num_eps = 20
@@ -43,5 +52,7 @@ if __name__ == '__main__':
     parser.add_argument('--no-render', default=False, action="store_true", help="Disable rendering")
     parser.add_argument('--max-timesteps', default=1000, type=int, help="Maximum number of timesteps")
     parser.add_argument('--model-type', default='mlp', type=str, help="Model type: cnn, mlp (default)")
+    parser.add_argument('--room-file', default='room_5x5_empty.txt', type=str,
+                        help="Room type: room_5x5_empty (default), 2_room_9x9")
     args = parser.parse_args()
     main(args)
